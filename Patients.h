d4bd89300d1336d78c9691eaebab51954e2de16b
Sabine825 <sabineelmahdy2004@gmail.com>
2025-03-31 23:56:03 +0200

    Create Patients.h

---
+++ Patients.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include"LinkedQueue.h"
+using namespace std;
+class Patients
+{
+private:
+	int PID;
+	int PT;
+	int VT;
+	string status;
+	string patienttype;
+	LinkedQueue<string>requiredtreatment;
+	bool IsRecv;
+	int duration[3];
+	int treatmentcount;
+
+public:
+	void setPID(int id);
+	void setPT(int appointementtime);
+	void setVT(int arrivaltime);
+	void setstatus(string s);
+	void setpatienttype(string t);
+	void AddTreatment(string &t, int d);
+	int getPID();
+	int getPT();
+	int getVT();
+	string getstatus();
+	string getpatienttype();
+
+
+};
+	
+
