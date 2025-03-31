bd9a4060bc39174484c70f58b9e2572b24978b03
Sabine825 <sabineelmahdy2004@gmail.com>
2025-03-31 23:57:03 +0200

    Create Patients.cpp

---
+++ Patients.cpp
@@ -0,0 +1,74 @@
+#include "Patients.h"
+#include<iostream>
+#include <string>
+using namespace std;
+
+void Patients::setPID(int id)
+{ 
+	PID = id;
+}
+
+void Patients::setPT(int appointementtime)
+{
+	PT = appointementtime;
+}
+
+void Patients::setVT(int arrivaltime)
+{
+	VT = arrivaltime;
+}
+
+void Patients::setstatus(string s)
+{
+	status = s;
+}
+
+void Patients::setpatienttype(string t)
+{
+	patienttype = t;
+}
+
+void Patients::AddTreatment(string &t, int d)
+{
+	int treatmentcount = 0;
+		if (treatmentcount >= 3)
+		{
+			return;
+		}
+		else
+		{
+			requiredtreatment.enqueue(t);
+			duration[treatmentcount] = d;
+			treatmentcount++;
+		}
+	
+}
+
+int Patients::getPID()
+{
+	return PID;
+}
+
+int Patients::getPT()
+{
+	return PT;
+}
+
+int Patients::getVT()
+{
+	return VT;
+}
+
+string Patients::getstatus()
+{
+	return status;
+}
+
+string Patients::getpatienttype()
+{
+	return patienttype;
+}
+
+
+
+
