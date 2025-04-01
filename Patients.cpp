46080269293e88144dbcd6ee8f208ac7f579531c
Sabine825 <sabineelmahdy2004@gmail.com>
2025-04-02 00:03:16 +0200

    Update Patients.cpp

--- Patients.cpp
+++ Patients.cpp
@@ -28,21 +28,28 @@ void Patients::setpatienttype(string t)
 	patienttype = t;
 }
 
-void Patients::AddTreatment(string &t, int d)
+void Patients::settreatmentcount(int c)
 {
-	int treatmentcount = 0;
-		if (treatmentcount >= 3)
-		{
-			return;
-		}
+	treatmentcount = c;
+}
+
+void Patients::AddTreatment(string n, int d)
+{ 
+
+
+	if (treatmentcount >= 3)
+	{
+		return;
+	}
 		else
 		{
-			requiredtreatment.enqueue(t);
-			duration[treatmentcount] = d;
+		Treatment NEWT(n,d);
+			requiredtreatment.enqueue(NEWT);
 			treatmentcount++;
+
 		}
+	}
 	
-}
 
 int Patients::getPID()
 {
@@ -69,28 +76,35 @@ string Patients::getpatienttype()
 	return patienttype;
 }
 
+int Patients::gettreatmentcount()
+{
+	return treatmentcount;
+}
+
 int Patients::penaltytime()
 {
 	if (VT > PT)
 	{
-		return ((PT + VT) / 2);
+		return (PT + VT) / 2;
 	}
 	else
 	{
-		return false;
+		return 0;
 	}
-ostream& operator<<(ostream& out, const Patients* patient) {
-    if (patient) {
-        out << "Patient ID: " << patient->PID
-            << ", Type: " << patient->patienttype
-            << ", Status: " << patient->status
-            << ", Arrival Time: " << patient->VT
-            << ", Appointment Time: " << patient->PT;
-    } else {
-        out << "Null Patient Pointer";
-    }
-    return out;
+	ostream& operator<<(ostream & out, const Patients * patient) {
+		if (patient) {
+			out << "Patient ID: " << patient->PID
+				<< ", Type: " << patient->patienttype
+				<< ", Status: " << patient->status
+				<< ", Arrival Time: " << patient->VT
+				<< ", Appointment Time: " << patient->PT;
+		}
+		else {
+			out << "Null Patient Pointer";
+		}
+		return out;
 }
+
 
 
 
