8984e917a5473c4db7b080f1692723932f6cfc9f
Sabine825 <sabineelmahdy2004@gmail.com>
2025-04-06 21:40:38 +0200

    Update and rename EarlyPatients to EarlyPatients.h

---
+++ EarlyPatients.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include"PriQueue.h"
+#include"Patients.h"
+using namespace std;
+class EarlyPatients
+{
+private:
+	priQueue<Patients*>ERLY;
+	string ListName = "ERLY";
+
+public:
+	void AddPatients(Patients* p);
+	void newlist(int currenttimestep);
+
+};
+
+
