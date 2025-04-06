d744ff37e596619367d59ce4ce0c2926580fea19
Belalmohamed110 <153289610+Belalmohamed110@users.noreply.github.com>
2025-04-06 15:24:56 -0700

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -1,16 +1,26 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include"Patients.h"
+#include"Resources.h"
+#include "LatePatients.h"
+#include"EarlyPatients.h"
+#include"priQueue.h"
+
 using namespace std;
 class Scheduler
 {
 	int current_time_step=0 ;
+	priQueue<EarlyPatients>early;
+	priQueue<LatePatients>late;
 public:
 	Scheduler();
-	void LoadPatientsFromFile(string );
-	void checkAllpatientslist();
-	void settime();
-	int gettime();
+	void LoadPatientstoAll(string filename);
+	void addtowaitlist(Patients & p);
+	//void checkAllpatientslist();
+	void randomwaiting();
+	/*void settime();
+	int gettime();*/
 	//void simulation();
 };
 
