6f05082437d4c012907811660f0c5aa1ba5bbc10
basel118200 <164511769+basel118200@users.noreply.github.com>
2025-04-07 16:25:53 +0200

    Update Resources.h

--- Resources.h
+++ Resources.h
@@ -1,17 +1,23 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include"Patients.h"
+#include "Patients.h"
 using namespace std;
+
 class Resources
 {
+private:
 	int ID;
-	string resourcetype ;
+	string resourcetype;
 	Patients* ppp;
+
 public:
 	Resources(int id, string rtype);
 	//bool IsEmpty(); msh 3aref lsa h3mlha ezay 
 	/*void assignresource( Patients * p);
 	void removeresource(Patients* p );*/
+
+	// Friend function declaration for operator<<
+	friend ostream& operator<<(ostream& os, const Resources& res);
 };
 
