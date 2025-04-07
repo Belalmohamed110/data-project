54ddd5a212a294d8b1ca93a216776b8880f38e65
basel118200 <164511769+basel118200@users.noreply.github.com>
2025-04-07 16:15:36 +0200

    Update Ui.h

--- Ui.h
+++ Ui.h
@@ -1,6 +1,11 @@
-#pragma once
+pragma once
 #include <windows.h>  // For Sleep() and system()
 #include <iostream>
+#include "StackADT.h"
+#include "QueueADT.h"
+#include "Patients.h"
+#include "Resources.h"
+
 class UI {
 public:
     UI();  // Constructor declaration
@@ -9,9 +14,12 @@ public:
     void printPatients(Patients* patientList[], int size);
     void printResource(const Resource& res);
     
-     template <typename T>
+    template <typename T>
     void printStack(const StackADT<T>& stack);
     
     template <typename T>
     void printStackCount(const StackADT<T>& stack);
+    
+    template <typename T>
+    void printQueueCount(const QueueADT<T>& queue);
 };
