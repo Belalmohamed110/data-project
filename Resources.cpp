18cd5d21b962f13fe00beaf948770b64dbbee26f
basel118200 <164511769+basel118200@users.noreply.github.com>
2025-04-07 16:26:43 +0200

    Update Resources.cpp

--- Resources.cpp
+++ Resources.cpp
@@ -1,8 +1,17 @@
 #include "Resources.h"
 
-Resources::Resources(int id, string rtype) :ID (id), resourcetype (rtype)
-{
+Resources::Resources(int id, string rtype) {
+    ID = id;
+    resourcetype = rtype;
+    ppp = nullptr;
 }
+
+// Operator<< implementation
+ostream& operator<<(ostream& os, const Resources& res) {
+    os << "Resource Type: " << res.resourcetype << ", ID: " << res.ID;
+    return os;
+}
+
 //bool Resources::IsEmpty()
 //{
 //    return false;
