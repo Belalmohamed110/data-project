e46fbc767166438ff1fcbbac229d19982d53b638
Sabine825 <sabineelmahdy2004@gmail.com>
2025-04-08 20:53:25 +0200

    Update Resources.cpp

--- Resources.cpp
+++ Resources.cpp
@@ -3,9 +3,28 @@
 Resources::Resources(int id, string rtype) {
     ID = id;
     resourcetype = rtype;
-    ppp = nullptr;
+    p = nullptr;
 }
 
+void Resources::setID(int id)
+{
+	ID = id;
+}
+
+void Resources::setresourcetype(string rtype)
+{
+	resourcetype = rtype;
+}
+
+int Resources::getID()
+{
+	return ID;
+}
+
+string Resources::getresourcetype()
+{
+	return resourcetype;
+}
 // Operator<< implementation
 ostream& operator<<(ostream& os, const Resources& res) {
     os << "Resource Type: " << res.resourcetype << ", ID: " << res.ID;
