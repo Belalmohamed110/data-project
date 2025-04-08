2d08f5750da04b34327b69205493d7fcf6fd47b8
basel118200 <164511769+basel118200@users.noreply.github.com>
2025-04-08 21:07:05 +0200

    Update Ui.h

--- Ui.h
+++ Ui.h
@@ -1,10 +1,13 @@
-pragma once
+#pragma once
 #include <windows.h>  // For Sleep() and system()
 #include <iostream>
 #include "StackADT.h"
 #include "QueueADT.h"
+#include "LinkedQueue.h"
+#include "priQueue.h"
 #include "Patients.h"
 #include "Resources.h"
+using namespace std;
 
 class UI {
 public:
@@ -13,13 +16,52 @@ public:
     void ClearConsole();
     void printPatients(Patients* patientList[], int size);
     void printResource(const Resource& res);
+    void printSectionHeader(const string& title);
+    
+    // Template functions must stay in header
+    template <typename T>
+    void printStack(const StackADT<T>& stack) {
+        stack.printStack();
+    }
     
     template <typename T>
-    void printStack(const StackADT<T>& stack);
+    void printStackCount(const StackADT<T>& stack) {
+        cout << stack.GetCountStack();
+    }
     
     template <typename T>
-    void printStackCount(const StackADT<T>& stack);
+    void printQueue(const LinkedQueue<T>& queue) {
+        queue.print();
+    }
     
     template <typename T>
-    void printQueueCount(const QueueADT<T>& queue);
+    void printQueueCount(const QueueADT<T>& queue) {
+        cout << queue.getCount();
+    }
+
+    template <typename T>
+    void printPriQueue(const priQueue<T>& pqueue) {
+        pqueue.print();
+    }
+    
+    template <typename T>
+    void printPriQueueCount(const priQueue<T>& pqueue) {
+        cout << pqueue.getCount();
+    }
+
+    // Print the entire system status
+    void printSystemStatus(
+        int currentTimestep,
+        const LinkedQueue<Patients>& allList,
+        const LinkedQueue<Patients>& eTherapyList,
+        const LinkedQueue<Patients>& uTherapyList,
+        const LinkedQueue<Patients>& xTherapyList,
+        const LinkedQueue<Patients>& earlyList,
+        const LinkedQueue<Patients>& lateList,
+        const LinkedQueue<Resource>& eDevices,
+        const LinkedQueue<Resource>& uDevices,
+        const LinkedQueue<Resource>& xRooms,
+        const LinkedQueue<Patients>& inTreatmentList,
+        const LinkedQueue<Patients>& finishedList
+    );
 };
