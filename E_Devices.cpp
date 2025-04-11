bb1553c4b93a67571cbf3ff049920750925140c9
Belalmohamed110 <153289610+Belalmohamed110@users.noreply.github.com>
2025-04-11 14:44:46 -0700

    Update E_Devices.cpp

--- E_Devices.cpp
+++ E_Devices.cpp
@@ -1,10 +1,13 @@
 #include "E_Devices.h"
-#include"E_WaitingList.h*
-
+#include"LinkedQueue.h"
+#include"E_WaitingList.h"
 E_Devices::E_Devices(int id) : Resources(id, "E_Device")
 {}
 
-bool E_Devices::enqueue(E_WaitingList* p) {
+bool E_Devices::enqueue(E_WaitingList * p)
+{
     if (isEmpty()) {
-             return LinkedQueue<E_WaitingList*>::enqueue(p);
+        return LinkedQueue<E_WaitingList*>::enqueue(p);
     }
+}
+
