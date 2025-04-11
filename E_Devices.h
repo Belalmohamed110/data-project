190697044cc27bdcbfe81b8207ff3a056440b597
Belalmohamed110 <153289610+Belalmohamed110@users.noreply.github.com>
2025-04-11 14:44:13 -0700

    Update E_Devices.h

--- E_Devices.h
+++ E_Devices.h
@@ -1,9 +1,10 @@
 #pragma once
 #include"Resources.h"
+#include"E_WaitingList.h"
 class E_Devices : public Resources
 {
 public:
 	E_Devices(int id);
-bool enqueue(E_WaitingList* p);
+bool enqueue(E_WaitingList * p);
 };
 
