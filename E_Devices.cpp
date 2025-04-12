bc45b129a4b961f23674616ef9df1dea8872142e
GloriaAyman <gloriaayman@icloud.com>
2025-04-13 00:19:42 +0200

    E_Devices.cpp

--- E_Devices.cpp
+++ E_Devices.cpp
@@ -1,13 +1,10 @@
 #include "E_Devices.h"
-#include"LinkedQueue.h"
 #include"E_WaitingList.h"
+
 E_Devices::E_Devices(int id) : Resources(id, "E_Device")
 {}
 
-bool E_Devices::enqueue(E_WaitingList * p)
-{
+bool E_Devices::enqueue(E_WaitingList* p) {
     if (isEmpty()) {
-        return LinkedQueue<E_WaitingList*>::enqueue(p);
+             return LinkedQueue<E_WaitingList*>::enqueue(p);
     }
-}
-
