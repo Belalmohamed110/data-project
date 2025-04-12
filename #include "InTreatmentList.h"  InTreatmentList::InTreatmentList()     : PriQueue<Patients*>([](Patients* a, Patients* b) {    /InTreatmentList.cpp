110149dde17cb76a4b4ec48c06d4252429c169c2
GloriaAyman <gloriaayman@icloud.com>
2025-04-13 00:44:32 +0200

    Delete #include "InTreatmentList.h"  InTreatmentList::InTreatmentList()     : PriQueue<Patients*>([](Patients* a, Patients* b) {     directory

--- #include "InTreatmentList.h"  InTreatmentList::InTreatmentList()     : PriQueue<Patients*>([](Patients* a, Patients* b) {    /InTreatmentList.cpp
+++
@@ -1,8 +0,0 @@
-#include "InTreatmentList.h"
-
-InTreatmentList::InTreatmentList()
-    : PriQueue<Patients*>([](Patients* a, Patients* b) {
-   // prioritize earlier finish times
-    return a->getCurrentTreatment()->getFinishTime() >
-        b->getCurrentTreatment()->getFinishTime();
-        }) 
