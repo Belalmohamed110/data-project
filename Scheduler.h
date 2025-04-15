2dcb0d4a09826fe33289ce038189befffb186a9a
Belalmohamed110 <153289610+Belalmohamed110@users.noreply.github.com>
2025-04-15 00:00:11 -0700

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -1,34 +1,45 @@
-#pragma once
-#include <iostream>
-#include <fstream>
+#include<iostream>
 #include"Patients.h"
 #include"Resources.h"
 #include "LatePatients.h"
+#include"AllPatients.h"
 #include"EarlyPatients.h"
+#include"E_WaitingList.h"
+#include <fstream>
+#include "U_WaitingList.h"
+#include"X_WaitingList.h"
+#include"LinkedQueue.h"
+#include"Node.h"
+#include"priNode.h"
 #include"priQueue.h"
+#include"StackADT.h"
+#include "ArrayStack.h"
 #include"QueueADT.h"
+#include<sstream>
 
 
 using namespace std;
 class Scheduler
 {
 	int current_time_step=0 ;
-	priQueue<EarlyPatients>early;
-	priQueue<LatePatients>late;
-	LinkedQueue<Patients> ALLpatients;
-	LinkedQueue<Patients> RandomWaiting;
-	LinkedQueue<Patients> InTreatment;
-	LinkedQueue<Patients> E_Waiting;
-	LinkedQueue<Patients> U_Waiting;
-	LinkedQueue<Patients> X_Waiting;
+	priQueue<Patients*>early;
+	priQueue<Patients*>late;
+	LinkedQueue<Patients*> ALLpatients;
+	LinkedQueue<Patients*> RandomWaiting;
+	LinkedQueue<Patients*> InTreatment;
+	LinkedQueue<Patients*> E_Waiting;
+	LinkedQueue<Patients*> U_Waiting;
+	LinkedQueue<Patients*> X_Waiting;
+	LinkedQueue<Patients*> finished;
+
 
 public:
 	Scheduler();
 	void LoadPatientstoAll(string filename);
 	void movefromALL(Patients * p);
 	//void checkAllpatientslist();
-	void randomwaiting();
-	void simulation(int max_time_steps);
+	void simulation();
+	//void simulation(int max_time_steps);
 	/*void settime();
 	int gettime();*/
 	//void simulation();
