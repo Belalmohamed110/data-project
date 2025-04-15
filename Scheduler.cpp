92e885e0df70a69700b6d97dd25bf285ff8ba54d
basel118200 <164511769+basel118200@users.noreply.github.com>
2025-04-15 19:56:50 +0200

    Update Scheduler.cpp

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -63,13 +63,13 @@ void Scheduler::LoadPatientstoAll(string filename) {
         char type;
         int id,pt, vt, nt;
         
-        iss >> type >> pt >> vt >> nt;
+        iss >> type >> id>>pt >> vt >> nt;
         
         string status = "New";
         string patientType = (type == 'N') ? "Normal" : "Recovering";
         
         // Create patient
-        Patients newPatient(id,pt, vt, patientType, status);
+        Patients *newPatient =new Patients (id,pt, vt, patientType, status);
         
         // Read treatments
         for (int j = 0; j < nt; j++) {
@@ -78,7 +78,7 @@ void Scheduler::LoadPatientstoAll(string filename) {
             iss >> treatmentType >> duration;
             
             treatment* newTreatment = new treatment(treatmentType, duration);
-            newPatient.AddTreatment(newTreatment);
+            newPatient->AddTreatment(newTreatment);
         }
         
         ALLpatients.enqueue(newPatient);
@@ -110,7 +110,18 @@ for (int i = 0; i < 30; i++) {
     }
 }
 }
+//void Scheduler::reschedule(int newpt, Patients*p1)
+//{
+//    Patients* ptr;
+//    while(ptr!=p1)
+//    { 
+//    int pri = -p1->getPT();
+//    p1 = early.head->getItem(pri);
+//    p1->setPT(newpt)
 //
+//
+//}
+////
 //void Scheduler::LoadPatientsFromDesktop(string fullPath) {
 //    cout << "Loading patients from: " << fullPath << endl;
 //    LoadPatientstoAll(fullPath);
@@ -125,36 +136,32 @@ for (int i = 0; i < 30; i++) {
 void Scheduler::simulation() {
     int x = rand() % 101; // number from 0 to 100
     //Patients p;
-    EarlyPatients *p;
-    E_WaitingList *e;
-    Patients *patient;
-    U_WaitingList *u;
-    X_WaitList *x;
+    
+    Patients *p;
+    p = ALLpatients.frontPtr->getItem();
 
     if (x < 33) {
         // E-Waiting
         if (!early.isEmpty()) {
  
-            p->newlist(current_time_step);
-            e->enqueue(patient);
-            patient->setstatus("E-Waiting");
+            //p->newlist(current_time_step);
+            E_Waiting.enqueue(p);
+            p->setstatus("E-Waiting");
         }
     }
     else if (x < 66 && x>=33) {
         // U-Waiting
         if (!early.isEmpty()) {
           
-            p->newlist(current_time_step);
-            u-> enqueue(patient);
-            patient->setstatus("E-Waiting");
+            U_Waiting.enqueue(p);
+            p->setstatus("E-Waiting");
         }
     }
     else {
         // X-Waiting
         if (!early.isEmpty()) {            
-            p->newlist(current_time_step);
-            x-> enqueue(patient);
-            patient->setstatus("E-Waiting");
+            X_Waiting.enqueue(p);
+            p->setstatus("E-Waiting");
         }
     }
 
@@ -163,62 +170,80 @@ void Scheduler::simulation() {
     if (X < 10) {
         //  patient from Early to RandomWaiting 
         if (!early.isEmpty()) {
-            EarlyPatients p;
-            int pri;
-            early.dequeue(p, pri);
-            RandomWaiting.enqueue(p);
-            p.setstatus("RandomWaiting from Early");
+            Patients * E;
+            int pri = -E->getPT();
+            E = early.head->getItem(pri);
+            RandomWaiting.enqueue(E);
+            E->setstatus("RandomWaiting from Early");
         }
     }
     else if (X < 20 && X>=10) {
         // patient from Late to RandomWaiting using (PT + penalty)
         if (!late.isEmpty()) {
-            late.dequeue(p);
-            p.setPT(p.getPT() + 5); 
-            RandomWaiting.enqueue(p);
-            p.setstatus("RandomWaiting from Late with penalty");
+            Patients* l; 
+            int pri = -l->getPT();
+            l= early.head->getItem(pri);
+            RandomWaiting.enqueue(l);
+            l->setstatus("RandomWaiting from Early"); 
         }
     }
     else if (X < 40 && X >= 20) {
         // 2 patients from RandomWaiting to InTreatment
         for (int i = 0; i < 2 && !RandomWaiting.isEmpty(); ++i) {
-            RandomWaiting.dequeue(p);
-            InTreatment.enqueue(p);
-            p.setstatus("In-Treatment");
+            Patients* p1;
+            Patients* p2;
+
+            p1= RandomWaiting.frontPtr->getItem();
+            p2 = RandomWaiting.frontPtr->getNext()->getItem();
+
+            InTreatment.enqueue(p1);
+            InTreatment.enqueue(p2);
+
+            p1->setstatus("In-Treatment");
+            p2->setstatus("In-Treatment");
+
         }
     }
     else if (X < 50 && X >= 40) {
         // iv. Move 1 patient from InTreatment to RandomWaiting
         if (!InTreatment.isEmpty()) {
-            InTreatment.dequeue(p);
-            RandomWaiting.enqueue(p);
-            p.setstatus("Back to RandomWaiting");
+            Patients* p1;
+            p1 = InTreatment.frontPtr->getItem();
+           /* InTreatment.dequeue(p1);*/
+            RandomWaiting.enqueue(p1);
+            p1->setstatus("Back to RandomWaiting");
         }
     }
     else if (X < 60 && X >= 50) {
         //  Move patient from InTreatment to Finish
         if (!InTreatment.isEmpty()) {
-            InTreatment.dequeue(p);
-            Finish.enqueue(p);
-            p.setstatus("Finished");
+            Patients* p1;
+            p1 = InTreatment.frontPtr->getItem();
+            InTreatment.dequeue(p1);
+            finished.enqueue(p1);
+            p1->setstatus("Finished");
         }
     }
     else if (X < 70 && X >= 60) {
         //  patient from X-Waiting to Finish
         if (!X_Waiting.isEmpty()) {
-            X_Waiting.dequeue(p);
-            Finish.enqueue(p);
-            p.setstatus("Finished from X-Waiting");
+            Patients* p1;
+            p1 = X_Waiting.frontPtr->getItem();
+           /* X_Waiting.dequeue(p1);*/
+            finished.enqueue(p1);
+            p1->setstatus("Finished from X-Waiting");
         }
     }
-    else if (X < 80 && X >= 70) {
+  /* else if (X < 80 && X >= 70) {
         if (!early.isEmpty()) {
-            early.dequeue(p);
-            p.setstatus("Rescheduled");
-            early.enqueue(p);
-        }
+            Patients* p1;
+            early.dequeue(p1,-p1->getPT());
+            p->setstatus("Rescheduled");
+            early.enqueue(p1);
+        }*/
+
     }
-}
+
 // yaraaaaaaab
 // kolo d5l fe b3do w msh 3aref da leh 3laka b phase 2 wla la 
 //void Scheduler::simulation(int max_time_steps) {
